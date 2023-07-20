6c5602448083fdac94a9e4996c03763683a2e1a8
Jose Cantero <jodacan10@gmail.com>
2023-07-20 16:15:06 -0500

    paused

---
+++ projects/redux-tutorial/src/pages/index.js
@@ -0,0 +1,12 @@
+import { ListOfUsers } from "../components/ListOfUsers";
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-center">Proyecto con Redux</h1>
+      <ListOfUsers className="w-5/6"></ListOfUsers>
+    </main>
+
+  )
+}
